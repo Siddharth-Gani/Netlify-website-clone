d327f291c7b32b766796f0577be19ae9c869fac1
Siddharth-Gani <152051824+Siddharth-Gani@users.noreply.github.com>
2023-11-26 14:03:21 +0530

    Add files via upload

---
+++ netlify.js
@@ -0,0 +1,30 @@
+let dev=document.getElementById('thirteen');
+dev.addEventListener('click',()=>{
+    let dev1=document.getElementById('fourteen');
+    dev1.style.backgroundColor="purple";
+    let std1=document.getElementById('one');
+    std1.style.color="orange";
+    let std2=document.getElementById('two');
+    std2.style.color="orange";
+    let std3=document.getElementById('three');
+    std3.style.color="orange";
+    let std4=document.getElementById('four');
+    std4.style.color="orange";
+    let std5=document.getElementById('five');
+    std5.style.color="orange";
+    let std6=document.getElementById('six');
+    std6.style.color="orange";
+    let std7=document.getElementById('ten');
+    std7.style.color="orange";
+    let std8=document.getElementById('eleven');
+    std8.style.color="orange";
+})
+let doc=document.getElementById('eighteen');
+doc.addEventListener("click",()=>{
+    window.location="https://unrivaled-cuchufli-4d20ac.netlify.app/";
+})
+let erp=document.getElementById('twentythree');
+erp.addEventListener("click",()=>{
+    let xrp=document.getElementById('twentyfour');
+    xrp.style.color="orange";
+})
